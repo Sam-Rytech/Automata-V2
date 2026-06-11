401b6d4ddfdbefc74591db5d7c72d742ecb77355
jadonamite <jadonamite@gmail.com>
2026-06-11 16:18:25 +0100

    docs: add TODO note for future optimization in AuthGuard.tsx

--- frontend/components/AuthGuard.tsx
+++ frontend/components/AuthGuard.tsx
@@ -1,4 +1,5 @@
 'use client';
+// TODO: consider memoizing this value
 import { usePrivy } from '@privy-io/react-auth';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';

4d44f8bb7e94d1f141857bf69f3a12de656fceb0
jadonamite <jadonamite@gmail.com>
2026-06-22 21:43:45 +0100

    feat(frontend/components): add index prop to ActionNode and fix Skele…

--- frontend/components/ui/skeleton.tsx
+++ frontend/components/ui/skeleton.tsx
@@ -1,1 +1,13 @@
-import { cn } from '@/lib/utils'; const DEFAULT_CLASS_NAMES = 'animate-pulse rounded-md bg-muted'; function Skeleton({ className, ...props }: React.ComponentProps<'div'>) { return ( <div data-slot='skeleton' className={cn(DEFAULT_CLASS_NAMES, className)} {...props} /> ); } export { Skeleton };
+import { cn } from "@/lib/utils"
+
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("animate-pulse rounded-md bg-muted", className)}
+      {...props}
+    />
+  )
+}
+
+export { Skeleton }
